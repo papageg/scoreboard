509bb554b9221e876ecaa07db09d5dede349bc6d
Greg <greggordon98@gmail.com>
2019-02-20 12:26:49 -0800

    up to date now
    up to date now

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Player from './Player';
+import AddPlayerForm from './AddPlayerForm';
 
 class App extends Component {
   state = {
@@ -27,11 +28,29 @@ class App extends Component {
       }
     ]
   };
+
+  // player id counter
+  prevPlayerId = 4;
       
   handleScoreChange = (index, delta) => {
     this.setState( prevState => ({
       score: prevState.players[index].score += delta
     }));
+  }
+
+  handleAddPlayer = (name) => {
+    this.setState ( prevState => {
+      return {
+        players: [
+          ...prevState.players,
+          {
+            name,
+            score: 0,
+            id: this.prevPlayerId += 1
+          }
+        ]
+      }
+    });
   }
 
   handleRemovePlayer = (id) => {
@@ -62,6 +81,8 @@ class App extends Component {
             removePlayer={this.handleRemovePlayer}           
           />
         )}
+
+        <AddPlayerForm addPlayer={this.handleAddPlayer}/>
       </div>
     );
   }
